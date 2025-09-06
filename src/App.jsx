1352ed9e3262854490640837e36bacd0ab206c38
Sayed Ahemmad <143632368+Sayeed-5@users.noreply.github.com>
2025-09-06 15:11:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Products from "./pages/Products"
+import LandingPage from './pages/LandingPage';
+import SigninPage from './pages/SignIn';
+import SellerSignup from './pages/Seller_signup';
+import AgriMart from './pages/AgriMart';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<SigninPage />} />
+          <Route path="/products" element={<Products />} /> 
+          <Route path="/seller" element={<SellerSignup />} /> 
+          <Route path="/agrimart" element={<AgriMart />} /> 
+        </Routes>
     </>
   )
 }
