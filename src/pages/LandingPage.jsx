b0b782232bea339ed80ee9d013502c732f6b78fe
Sayed Ahemmad <143632368+Sayeed-5@users.noreply.github.com>
2025-09-06 15:05:16 +0530

    Create LandingPage.jsx

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,29 @@
+import { TooltipProvider } from "../components/ui/tooltip"
+import Hero from "../components/Hero"
+import Features from "../components/Features"
+import FarmerDashboard from "../components/FarmerDashboard"
+import LocalBuyersLogistics from "../components/LocalBuyersLogistics"
+import Leaderboard from "../components/LeaderBoard"
+import Footer from "../components/Footer"
+
+
+function LandingPage() {
+    return (
+      <TooltipProvider>
+        <div className="max-w-[100rem] mx-auto bg-fixed bg-cover bg-center " style={{ backgroundImage: "url('/Rice-Field.jpg')"}}>
+          <div className="bg-black/30 min-h-screen">
+            <main>
+              <Hero />
+              <Features />
+              <FarmerDashboard />
+              <Leaderboard />
+              <LocalBuyersLogistics />
+            </main>
+            <Footer />
+          </div>
+        </div>
+      </TooltipProvider>
+    )
+  }
+  
+  export default LandingPage
